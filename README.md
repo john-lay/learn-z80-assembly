737a6b022c6041271c853c374778565575f71ef7
john-lay <john_lay@hotmail.co.uk>
2020-03-10 20:56:10 +0000

    Implement division from tutorial

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Play with  labels and a loop to fill VRAM.
 ## Lesson 3 - Case Statement, 8-bit arithmetic
 
 Basic artithmetic calculations (addition and subtraction).
-Uses labels, label values (EQU), relative jump (jr), compare (cp), compare to zero (z), addition (add) and subtraction (sub)
+Uses labels, label values (EQU), relative jump (jr), compare (cp), compare to zero (z), no-carry (nc), addition (add) and subtraction (sub)
 The DJNZ (Decrement, Jump if Not Zero) (only works in the `b` register) instruction is missing from the Gameboy instruction set.
 
 ![screenshot of lesson3](https://github.com/john-lay/learn-z80-assembly/raw/master/lesson3/lesson3-screenshot.png)
--- lesson3/main.asm
+++ lesson3/main.asm
@@ -17,12 +17,14 @@ entryPoint:
 _ADD EQU 0
 _SUB EQU 1
 _MUL EQU 2
+_DIV EQU 3
 
 init:
     ; ld a, _ADD      ; load _ADD label into a (this is the command we want to execute)
     ; ld a, _SUB      ; load _SUB label into a (this is the command we want to execute)
-    ld a, _MUL      ; load _MUL label into a (this is the command we want to execute)
-    ld bc, $0307    ; load 3 into b and 7 into c
+    ; ld a, _MUL      ; load _MUL label into a (this is the command we want to execute)
+    ld a, _DIV      ; load _DIV label into a (this is the command we want to execute)
+    ld bc, $030c    ; load 3 into b and 12 into c
 
     cp 0            ; compare a to zero
     jr z, MathAdd   ; relative jump
@@ -30,6 +32,8 @@ init:
     jr z, MathSub    
     cp 2            ; compare a to two
     jr z, MathMul
+    cp 3            ; compare a to three
+    jr z, MathDiv
 
     ld a, 0
 
@@ -39,7 +43,7 @@ SaveResult:
     ret
 
 LoadAndSaveResult:
-    ld a, d        ; read result from c
+    ld a, d        ; read result from d
     ld [_RAM], a    ; Save the result to working ram C000 (in hex)
     
     ret
@@ -56,6 +60,7 @@ MathAdd:
 
 MathMul:            ; Example b=3, c=7
     ld a, c         ; load c into a (a=7, b=3, c=7, d=0)
+
     add c           ; add c and a   (a=14, b=3, c=7, d=0)
     ld d, a         ; load a into d (a=14, b=3, c=7, d=14)
     ld a, b         ; load b into a (a=14, b=3, c=7, d=14)
@@ -67,11 +72,24 @@ MathMul:            ; Example b=3, c=7
 MathMulAgain:
     ld b, a         ; load a into b (a=2, b=2, c=7, d=14)
     ld a, d         ; load d into a (a=14, b=2, c=7, d=14)
-    add c           ; add c and a   (a=21, b=2, c=7, d=14)
 
+    add c           ; add c and a   (a=21, b=2, c=7, d=14)
     ld d, a         ; load a into d (a=21, b=2, c=7, d=21)
     ld a, b         ; load b into a (a=2, b=2, c=7, d=21)
     dec a           ; decrement a   (a=1, b=2, c=7, d=21)
     cp 1            ; compare a to 1
     jr z, LoadAndSaveResult
     jr nz, MathMulAgain
+
+MathDiv:            ; Example b=3, c=12
+    ld a, c         ; (a=12, b=3, c=12, d=0)
+    cp 0
+    jr z, SaveResult
+    ld d, 0
+MathDivAgain:
+    sub b           ; (a=9, b=3, c=12, d=0)
+    inc d           ; (a=9, b=3, c=12, d=1)
+    jr nc, MathDivAgain
+    dec d
+    ld a, d
+    jr SaveResult
