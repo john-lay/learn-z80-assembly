0ee78d5c8cba6ea82147ed1b2bbc830a0bb34d1f
john-lay <john_lay@hotmail.co.uk>
2020-03-21 15:41:30 +0000

    Write string messages to working RAM

--- lesson4/main.asm
+++ lesson4/main.asm
@@ -31,11 +31,18 @@ init:
     ; ld a, [charA]   ; load 'a' into a (a=97d, a=0x61)
     call printChar
 
+    ld de, thoughtMsg
+    call printString
+    ld de, awesomeMsg
+    call printString
+    
     ret
 
 charA: db $61       ; define byte (db) with value 'a' (charA=97d, charA=0x61)
 charX: db $78       ; define byte (db) with value 'x' (charX=120d, charX=0x78)
 charBar: db $7c     ; define byte (db) with value '|' (charBar=124d, charBar=0x7c)
+thoughtMsg: db "Thought of the day...", 255     ; string ends with 255
+awesomeMsg: db "Z80 is Awesome!", 255           ; use 255 as terminating string
 
 initSave:
     ld hl, _RAM     ; initialise hl with memory location of _RAM (0xc000)
@@ -47,3 +54,11 @@ printChar:
     inc hl          ; set hl to next available memory location (ready for next save)
 
     ret
+
+printString:
+    ld a, [de]
+    ld [hli], a ; Place it at the destination, incrementing hl
+    inc de      ; move to the next character to print by incrementing de
+    cp 255      ; check if we're at the end of the string
+    ret z
+    jp printString
